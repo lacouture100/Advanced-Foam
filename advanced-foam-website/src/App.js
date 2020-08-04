f3e90be3687129ee97bfc751f3fc41109f6d6ffe
Alvaro Lacouture <lacouture100@gmail.com>
2020-08-04 12:17:56 -0400

    Check upload

--- advanced-foam-website/src/App.js
+++ advanced-foam-website/src/App.js
@@ -15,6 +15,60 @@ class App extends Component {
     }
   }
 
+
+  componentDidMount(){
+
+
+    setInterval(() => {
+        //console.log(this.state.data);
+        //console.log(Object.keys(this.state.data).length)
+        /*// Cycle through every title in our data
+
+        Object.keys(this.state.data).forEach((value,index,array)=>{
+            // array is the array of categories
+            console.log(`These are our array of categories: ${array}`)
+            var arrayLength = array.length;
+            for (var i = 0; i < arrayLength; i++) {
+                // This is the array of categories inside our data
+                //console.log(data[`${array[i]}`])
+                const innerArrayLength = data[`${array[i]}`].length;
+                // Print the title for each of our elements in each category array
+                for(var j=0; j < innerArrayLength; j++){
+                    console.log(data[`${array[i]}`][j]._id);
+                }
+            }
+        })*/
+//console.log(this.state.studioProperty.index);
+        //console.log((data.Studio.length - 1));
+
+
+        let newIndex = this.state.property.index;
+        let studioLength = data.properties.length-1;
+
+        if( this.state.property.index !== (studioLength) ){
+            newIndex += 1;
+            console.log("Changing image");
+        }else {
+            console.log("Setting the first image");
+            newIndex=1;
+        }
+
+        this.setState({
+            property: data.properties[newIndex]
+
+        })
+
+        
+      }, 3000);
+
+      
+        
+}
+
+
+
+
+
   toggleAppear = () => {
     this.setState({
       appearHome: !this.state.appearHome
@@ -42,6 +96,24 @@ class App extends Component {
         <button onClick={() => this.toggleAppear()}>Appear: {`${appearHome}`}</button>
         <button onClick={() => this.nextProperty()} disabled={property.index === data.properties.length-1}>Next</button>
         <button onClick={() => this.prevProperty()} disabled={property.index === 0}>Prev</button>
+
+        <CSSTransition
+          in={appearHome}
+          appear={true}
+          timeout={1000}
+          classNames="fade"
+        >
+          <Home property={property} />
+        </CSSTransition>
+
+        <CSSTransition
+          in={appearHome}
+          appear={true}
+          timeout={1000}
+          classNames="fade"
+        >
+          <Home property={property} />
+        </CSSTransition>
 
         <CSSTransition
           in={appearHome}
