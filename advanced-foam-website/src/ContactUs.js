4acf02fa690408e85452eb055c766deed3985673
Alvaro Lacouture <lacouture100@gmail.com>
2020-08-04 23:39:33 -0400

    Set main page style

--- advanced-foam-website/src/ContactUs.js
+++ advanced-foam-website/src/ContactUs.js
@@ -8,10 +8,13 @@ const ContactUs = () =>  {
   
     return(
       <div className="contact-main">
+        <div className="contact-border"></div>
         <img src="./img/contactUs.png"
              alt="Contact Us Button"
              className="contact-img"
-             />
+             >
+              
+               </img>
       </div>
     )
   }
