89ce5c55744418ac3087677ca0323538145cedf6
Alvaro Lacouture <lacouture100@gmail.com>
2020-08-18 17:59:37 -0400

    Added exterior links to clients

--- advanced-foam-website/src/components/Clients/Clients.js
+++ advanced-foam-website/src/components/Clients/Clients.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Clients.css';
+import { BrowserRouter as Router,Link,NavLink,withRouter } from 'react-router-dom';
 
 import data from '../../data/clients_data'
 
@@ -11,18 +12,27 @@ class Clients extends Component {
     this.state = {
       data : data.Clients
     }
+    this.goToOutsideLink = this.goToOutsideLink.bind(this);
   }
+
+  
+  goToOutsideLink(link){
+    window.location.href =link;
+}
 
   render(){
       const clients = this.state.data.map((client,index) =>(
         <div key={index}className="client">
+          <a href={client.link}>
           <img src={client.imgSrc}
               alt={client.imgAlt}
               className={`${client.name}-img client-img`}
               />
+              </a>
   <h4 className="client-name">{client.name}</h4>
         </div>
       ))
+      
     return(
       <div className="clients-main">
         <h3 className="clients-main-title"> 
@@ -38,4 +48,4 @@ class Clients extends Component {
   }
 }
 
-export default Clients;
+export default withRouter(Clients);
