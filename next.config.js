93c683e60b6c4920fa2a25e0da77864fe7fc4905
Alvaro Lacouture <lacouture100@gmail.com>
2020-09-13 23:03:46 -0400

    made serverless

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 const withPlugins = require('next-compose-plugins');
-const withCSS = require('@zeit/next-css');
-const withImages = require('next-images');
+//const withCSS = require('@zeit/next-css');
+//const withImages = require('next-images');
 
+module.exports = {
+  target: 'serverless',
+  // Other options are still valid
+};
+/*
 module.exports = withPlugins([
-    [withCSS],
-    [withImages]
-  ]);
+  [withCSS],
+  [withImages]
+]);
 
 
-
+*/
